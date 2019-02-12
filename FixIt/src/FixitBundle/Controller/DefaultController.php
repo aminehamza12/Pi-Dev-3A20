479ea709640c9e7f7f255c93e00582de2ea97f9c
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2019-02-12 21:54:58 +0100

    inetgration back

--- FixIt/src/FixitBundle/Controller/DefaultController.php
+++ FixIt/src/FixitBundle/Controller/DefaultController.php
@@ -2,10 +2,12 @@
 
 namespace FixitBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
     public function indexAction()
     {
         $user=null;
@@ -14,5 +16,30 @@ class DefaultController extends Controller
             $user = $this->container->get('security.token_storage')->getToken()->getUser();
         }
         return $this->render('@Fixit/Default/index.html.twig',array('user'=>$user));
+    }
+
+    public function indexAdminAction()
+    {
+        $user=null;
+        if( $this->container->get( 'security.authorization_checker' )->isGranted( 'IS_AUTHENTICATED_FULLY' ) )
+        {
+            $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        }
+        if(in_array('ROLE_ADMIN',$user->getRoles()))
+        {
+            return $this->render('@Fixit/back/index.html.twig',array('user'=>$user));
+        }
+        return $this->redirectToRoute('fixit_404');
+    }
+
+    public function wrongAction()
+    {
+        $user=null;
+        if( $this->container->get( 'security.authorization_checker' )->isGranted( 'IS_AUTHENTICATED_FULLY' ) )
+        {
+            $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        }
+
+        return $this->render('@Fixit/Default/404.html.twig',array('user'=>$user));
     }
 }
