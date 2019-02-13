116c26f0a198d0fac881c8544011cace7a885b22
aminehamza12 <you@example.com>
2019-02-13 21:36:02 +0100

    Entity

---
+++ FixIt/src/FixitBundle/Entity/Competences.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FixitBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Competences
+ *
+ * @ORM\Table(name="competences")
+ * @ORM\Entity(repositoryClass="FixitBundle\Repository\CompetencesRepository")
+ */
+class Competences
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Competences
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
