d9e32321a5a4d2530fc0f1788232486fabe5c4a4
nadim <nadim.jemai@esprit.tn>
2019-02-25 05:23:42 +0100

    conf

--- FixIt/app/AppKernel.php
+++ FixIt/app/AppKernel.php
@@ -17,6 +17,17 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new FixitBundle\FixitBundle(),
+            new FOS\CommentBundle\FOSCommentBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new \Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new FOS\MessageBundle\FOSMessageBundle(),
+            new AVATOR\TurbosmsBundle\AVATORTurbosmsBundle()
+            // new blackknight467\StarRatingBundle\StarRatingBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
